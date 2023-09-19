aa3ecaff3c045a59d5bc6d18f963ae2f0c00f842
kangjunkyu <126400738+kangjunkyu@users.noreply.github.com>
2023-09-20 01:12:03 +0900

    [Gold V] Title: 토마토, Time: 592 ms, Memory: 120604 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.java
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -25,12 +25,14 @@ public class Main {
 		for (int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
 			for (int j = 0; j < M; j++) {
+				//1을 찾아서 스택에 넣어야 한다. 그래야 비교를 하며 탐색을 돌릴 수 있으니
 				box[i][j] = Integer.parseInt(st.nextToken());
 				if (box[i][j] == 1) {
 					que.add(new int[] { i, j });
 				}
 			}
 		}
+		//반환값을 int로 받는게 정석인듯, 가장 큰 자리에서 자리에서 출력할거니까 
 		System.out.println(bfs());
 
 	}
@@ -46,10 +48,12 @@ public class Main {
 			for (int i = 0; i < 4; i++) {
 				int nx = x + dx[i];
 				int ny = y + dy[i];
-
+				
+				//컨티뉴를 시켜라.. 좀..
 				if (nx < 0 || nx >= N || ny < 0 || ny >= M) {
 					continue;}
 				else {
+					// 익은 토마토 사방에 안익은 토마토가 있다고?! 그럼 바꿔야지
 					if (box[nx][ny] == 0) {
 						que.add(new int[] { nx, ny });
 						box[nx][ny] = box[x][y] + 1;
@@ -59,7 +63,8 @@ public class Main {
 
 		}
 		max = Integer.MIN_VALUE;
-
+		
+		//만약에 전부 돌렸을 때 다 0이면 -1를 출력해야한다.
 		for (int i = 0; i < N; i++) {
 			for (int j = 0; j < M; j++) {
 				if (box[i][j] == 0) {
@@ -69,6 +74,7 @@ public class Main {
 					max = box[i][j];
 			}
 		}
+		//다 익었어..? 그럼 0 출력해 뭣하러 해 빨리 팔아야
 		if (max == 1) {
 			return 0;
 		} else {
