2e4858219918ac4cb167b478b5aa1ed48333552c
kangjunkyu <126400738+kangjunkyu@users.noreply.github.com>
2023-09-24 20:56:38 +0900

    [Gold IV] Title: 네트워크 연결, Time: 544 ms, Memory: 51740 KB -BaekjoonHub

---
+++ 백준/Gold/1922. 네트워크 연결/네트워크 연결.java
@@ -0,0 +1,79 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main {
+	static class Node implements Comparable<Node> {
+		public Node(int ed, int w) {
+			super();
+			this.ed = ed;
+			this.w = w;
+		}
+
+		int ed;
+		int w;
+
+		@Override
+		public int compareTo(Node o) {
+			return this.w - o.w;
+		}
+	}
+
+	static int N, M, sum;
+	static boolean check[];
+	static ArrayList<ArrayList<Node>> graph;
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+
+		N = Integer.parseInt(br.readLine());
+		M = Integer.parseInt(br.readLine());
+		sum = 0;
+
+		graph = new ArrayList<>();
+		check = new boolean[N+1];
+		for (int i = 0; i <= N; i++) {
+			graph.add(new ArrayList<>());
+		}
+
+		for (int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			int c = Integer.parseInt(st.nextToken());
+
+			graph.get(a).add(new Node(b, c));
+			graph.get(b).add(new Node(a, c));
+		}
+
+		prim(new Node(1, 0));
+		
+		System.out.println(sum);
+	}
+
+	static void prim(Node start) {
+		PriorityQueue<Node> que = new PriorityQueue<>();
+
+		que.add(start);
+
+		while (!que.isEmpty()) {
+			Node now = que.poll();
+			if (!check[now.ed]) {
+				check[now.ed] = true;
+
+				sum += now.w;
+
+				for (Node next : graph.get(now.ed)) {
+//					if (!check[next.ed]) {
+						que.add(next);
+//					}
+				}
+			}
+		}
+	}
+
+}
