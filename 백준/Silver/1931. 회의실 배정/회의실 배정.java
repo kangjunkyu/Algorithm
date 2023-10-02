37e5ee9cf057e2cb2bab65802f80e894fadc8a07
kangjunkyu <126400738+kangjunkyu@users.noreply.github.com>
2023-10-02 14:52:56 +0900

    [Silver I] Title: 회의실 배정, Time: 468 ms, Memory: 42244 KB -BaekjoonHub

---
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.java
@@ -0,0 +1,50 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+
+		int N = Integer.parseInt(br.readLine());
+
+		int[][] arr = new int[N][2];
+
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			arr[i][0] = Integer.parseInt(st.nextToken());
+			arr[i][1] = Integer.parseInt(st.nextToken());
+
+		}
+
+		Arrays.sort(arr, new Comparator<int[]>() {
+
+			@Override
+			public int compare(int[] o1, int[] o2) {
+				// TODO Auto-generated method stub
+				if (o1[1] == o2[1])
+					return o1[0] - o2[0];
+				return o1[1] - o2[1];
+			}
+
+		});
+		
+		int count = 0;
+		int end = 0;
+		
+		for (int i = 0; i < N; i++) {
+			//만약 다른 배열의 끝나는 시간이 시작시간보다 작다면
+			// end는 회의가 끝나는 시간으로 변경
+			if(end <= arr[i][0]) {
+				end = arr[i][1];
+				count++;
+			}
+		}
+		System.out.println(count);
+	}
+}
