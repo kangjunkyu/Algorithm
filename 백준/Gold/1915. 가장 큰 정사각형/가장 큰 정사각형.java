7568346b1f7e1b2b49cddacf91c31b679717a861
Junkyu Kang <126400738+kangjunkyu@users.noreply.github.com>
2024-07-28 14:29:24 +0900

    [Gold IV] Title: 가장 큰 정사각형, Time: 256 ms, Memory: 25944 KB -BaekjoonHub

--- 백준/Gold/1915. 가장 큰 정사각형/가장 큰 정사각형.java
+++ 백준/Gold/1915. 가장 큰 정사각형/가장 큰 정사각형.java
@@ -27,6 +27,7 @@ public class Main {
 		}
 		checkSquare(square);
 
+		// 한변의 길이의 제곱이 면이니까.
 		System.out.println((int)Math.pow(max, 2));
 
 	}
@@ -34,11 +35,15 @@ public class Main {
 	private static void checkSquare(int[][] square) {
 		dp = new int[N + 1][M + 1];
 
+		// dp 비교를 왼쪽대각선으로 고려해서 봐야한다.
+		// 아래로 보고 가면 안됨..
 		for (int i = 1; i <= N; i++) {
 			for (int j = 1; j <= M; j++) {
+				// 0이면 0이다.
 				if (square[i][j] == 0) {
 					dp[i][j] = 0;
 				} else {
+					// 만약 dp가 1이고, 나른 곳도 다 1이다? 그러면 +1을 해줘서 2로 만들어주면 한변의 길이가 나온다.
 					dp[i][j] = Math.min(dp[i][j - 1], Math.min(dp[i - 1][j - 1], dp[i - 1][j])) + 1;
 				}
 				max = Math.max(max, dp[i][j]);
