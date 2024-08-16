8368d17d9796bdee82504ab8582b42188b928cc8
Junkyu Kang <126400738+kangjunkyu@users.noreply.github.com>
2024-08-16 17:33:10 +0900

    [Silver I] Title: 경로 찾기, Time: 124 ms, Memory: 13924 KB -BaekjoonHub

---
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+
+		int N = Integer.parseInt(br.readLine());
+
+		int[][] arr = new int[N][N];
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < N; j++) {
+				arr[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				for (int j2 = 0; j2 < N; j2++) {
+					if (arr[j][i] == 1 && arr[i][j2] == 1) {
+						arr[j][j2] = 1;
+					}
+				}
+			}
+		}
+		StringBuilder sb = new StringBuilder();
+
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				sb.append(arr[i][j]).append(" ");
+			}
+			sb.append('\n');
+		}
+		System.out.println(sb);
+	}
+}
