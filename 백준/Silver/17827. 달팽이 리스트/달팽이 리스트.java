57b824f9db45a8528fd68192c966cd02232ce7e0
Junkyu Kang <126400738+kangjunkyu@users.noreply.github.com>
2024-09-24 19:59:09 +0900

    [Silver II] Title: 달팽이 리스트, Time: 624 ms, Memory: 52172 KB -BaekjoonHub

---
+++ 백준/Silver/17827. 달팽이 리스트/달팽이 리스트.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+	        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	        StringTokenizer st = new StringTokenizer(br.readLine());
+	        int N = Integer.parseInt(st.nextToken());
+	        int M = Integer.parseInt(st.nextToken());
+	        int V = Integer.parseInt(st.nextToken());
+	        int[] arr = new int[N + 1];
+	        st = new StringTokenizer(br.readLine());
+	        for (int i = 1; i <= N; i++) {
+	            arr[i] = Integer.parseInt(st.nextToken());
+	        }
+
+	        StringBuilder sb = new StringBuilder();
+	        for (int i = 0; i < M; i++) {
+	            int num = Integer.parseInt(br.readLine())+1;
+	            if (num <= N) {
+	                sb.append(arr[num]).append("\n");
+	            } else {
+	                int tmp = (num - (V-1)) % (N - (V-1));
+	                if (tmp == 0) {
+	                    sb.append(arr[N]).append("\n");
+	                } else {
+	                    sb.append(arr[tmp + (V-1)]).append("\n");
+	                }
+	            }
+	        }
+
+	        System.out.print(sb);
+
+	    }
+}
