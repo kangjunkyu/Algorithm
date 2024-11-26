c99bf63ca457987fcd110e34970fa5f994ec4712
Junkyu Kang <126400738+kangjunkyu@users.noreply.github.com>
2024-11-26 23:31:33 +0900

    [Gold IV] Title: 부분합, Time: 248 ms, Memory: 24368 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.java
+++ 백준/Gold/1806. 부분합/부분합.java
@@ -7,32 +7,37 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
-		
+
 		st = new StringTokenizer(br.readLine());
-		
+
 		int N = Integer.parseInt(st.nextToken());
 		int S = Integer.parseInt(st.nextToken());
-		
-		int[] num = new int[N+1];
-		st= new StringTokenizer(br.readLine());
+
+		int[] num = new int[N + 1];
+		st = new StringTokenizer(br.readLine());
 		for (int i = 0; i < N; i++) {
 			num[i] = Integer.parseInt(st.nextToken());
 		}
-		
+
 		int start = 0;
 		int end = 0;
 		int sum = 0;
-		
+
 		int ans = Integer.MAX_VALUE;
-		while(start <= N && end <= N) {
-			if(sum < S) {
+
+		// 슬라이딩 윈도우 방법
+		// 기본적으로 start와 end는 결코 N을 넘어선 안됨, 배열의 부분합이니까
+		while (start <= N && end <= N) {
+			// 부분합이 S보다 작다면 end를 늘린다.
+			if (sum < S) {
 				sum += num[end++];
-			}else {
-				ans = Math.min(ans, end-start);
+			} else {
+				// 아니라면 start를 늘린다. 
+				ans = Math.min(ans, end - start);
 				sum -= num[start++];
 			}
 		}
-		
+
 		System.out.println(ans == Integer.MAX_VALUE ? 0 : ans);
 	}
 }
