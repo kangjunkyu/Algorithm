f3c0d23bd5888ddb4fab2272358c10bd4987a92e
Junkyu Kang <126400738+kangjunkyu@users.noreply.github.com>
2024-12-09 10:27:09 +0900

    Time: 5 ms (62.91%) | Memory: 44.8 MB (31.96%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.java
+++ 13-roman-to-integer/roman-to-integer.java
@@ -11,13 +11,13 @@ class Solution {
 
         int ans = 0;
 
-        for(int i = 0; i < s.length(); i++){
-            if(i < s.length()-1 && map.get(s.charAt(i)) < map.get(s.charAt(i+1))){
+        for(int i = 0; i < s.length()-1; i++){
+            if(map.get(s.charAt(i)) < map.get(s.charAt(i+1))){
                 ans -=map.get(s.charAt(i));
             }else{
                 ans += map.get(s.charAt(i));
             }
         }
-        return ans;
+        return ans + map.get(s.charAt(s.length()-1));
     }
 }
