ea5008392e4a19062c6de11733535fd673081675
Junkyu Kang <126400738+kangjunkyu@users.noreply.github.com>
2025-08-29 09:09:29 +0900

    [level 1] Title: [PCCE 기출문제] 10번 / 공원, Time: 5.27 ms, Memory: 90.7 MB -BaekjoonHub

--- 프로그래머스/1/340198. ［PCCE 기출문제］ 10번 ／ 공원/［PCCE 기출문제］ 10번 ／ 공원.java
+++ 프로그래머스/1/340198. ［PCCE 기출문제］ 10번 ／ 공원/［PCCE 기출문제］ 10번 ／ 공원.java
@@ -1,44 +1,33 @@
-import java.util.Arrays;
-
+import java.util.*;
 class Solution {
     public int solution(int[] mats, String[][] park) {
-        int row = park.length;
-        int col = park[0].length;
-
-        Arrays.sort(mats); // 가장 큰 돗자리부터 확인하기 위해 정렬
-        int max = -1;
-
-        // 가장 큰 돗자리부터 시도
-        for (int t = mats.length - 1; t >= 0; t--) {
-            int size = mats[t];
-            boolean found = false;
-
-            // park를 순회하며 size x size 크기의 공간 검사
-            for (int i = 0; i <= row - size; i++) {
-                for (int j = 0; j <= col - size; j++) {
-                    boolean check = true;
-                    // size x size 내에 "-1" 이외의 문자가 있는지 확인
-                    for (int x = i; x < i + size && check; x++) {
-                        for (int y = j; y < j + size; y++) {
-                            if (!park[x][y].equals("-1")) {
-                                check = false;
-                                break;
-                            }
-                        }
-                    }
-
-                    if (check) {
-                        max = size;
-                        found = true;
-                        break;
-                    }
+        int answer = -1;
+        
+        for(int m : mats) {
+          for(int i=0; i<park.length; i++) {
+            for(int j=0; j<park[i].length; j++) {
+                if(park[i][j].equals("-1")) {
+                    if(matchSquare(i, j, m, park)) {
+                      answer = Math.max(answer, m);  
+                    } 
                 }
-                if (found) break;
             }
-
-            if (max != -1) break;
+          }  
         }
-
-        return max;
+        
+        return answer;
+    }
+    
+    private boolean matchSquare(int x, int y, int answer, String[][] park) {
+       if(x+answer-1 >= park.length || y+answer-1 >= park[x].length) return false;
+        
+       for(int i=0; i<answer; i++) {
+            for(int j=0; j<answer; j++) {
+                String s = park[x+i][y+j];
+                if(!s.equals("-1")) return false;    
+            }
+        } 
+        
+        return true;
     }
 }
