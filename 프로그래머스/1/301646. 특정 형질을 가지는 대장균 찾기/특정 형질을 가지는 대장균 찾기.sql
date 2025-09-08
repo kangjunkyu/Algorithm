dfa7ff711888e465cf2e829699d521a0392d21b7
Junkyu Kang <126400738+kangjunkyu@users.noreply.github.com>
2025-09-08 23:30:49 +0900

    [level 1] Title: 특정 형질을 가지는 대장균 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/1/301646. 특정 형질을 가지는 대장균 찾기/특정 형질을 가지는 대장균 찾기.sql
+++ 프로그래머스/1/301646. 특정 형질을 가지는 대장균 찾기/특정 형질을 가지는 대장균 찾기.sql
@@ -1,4 +1,5 @@
--- 코드를 작성해주세요
-
-select count(*) as COUNT from ecoli_data
-    where (GENOTYPE & 2) != 2 AND ((GENOTYPE & 4) = 4 OR (GENOTYPE & 1) = 1); 
+SELECT  COUNT(*)  AS COUNT
+  FROM  ECOLI_DATA A
+ WHERE  1=1
+        AND (GENOTYPE & 2) != 2 
+        AND ((GENOTYPE & 4) = 4 OR (GENOTYPE & 1) = 1)
